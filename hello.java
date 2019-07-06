d0d388e263a13ac881e81528ec7da3e27f04fe3b
Soumitra Mandal <soumitramandal1999@gmail.com>
2019-07-06 22:41:30 +0530

    java files first commit

---
+++ hello.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class MyClass{
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    Scanner myVar = new Scanner(System.in);
+    System.out.println(myVar.nextLine());
+  }
+}
